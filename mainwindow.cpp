eb9c71e3f3223ab62df0ba3cab7665544eebe99c
Francesco <francesco.argentieri89@gmail.com>
2017-09-09 23:33:14 +0200

    :sparkles: fix missed input file
    -verify correct input file
    -launch information message

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,71 +37,78 @@ void MainWindow::on_LoadFile_clicked()
 {
     qDebug()<<"Open file dialog...";
     fileName = QFileDialog::getOpenFileName(this,tr("Open file"), "", tr("All Files (*.*)"));
-    ui->lineEdit->setText(fileName);
+    if(fileName != "")
+    {
+        ui->lineEdit->setText(fileName);
 
-    // Create a progress dialog.
-    QProgressDialog dialog;
-    dialog.setLabelText(QString("Reading file..."));
+        // Create a progress dialog.
+        QProgressDialog dialog;
+        dialog.setLabelText(QString("Reading file..."));
 
-    // Create a QFutureWatcher and connect signals and slots.
-    QFutureWatcher<void> futureWatcher;
-    QObject::connect(&futureWatcher, SIGNAL(finished()), &dialog, SLOT(reset()));
-    QObject::connect(&dialog, SIGNAL(canceled()), &futureWatcher, SLOT(cancel()));
-    QObject::connect(&futureWatcher, SIGNAL(progressRangeChanged(int,int)), &dialog, SLOT(setRange(int,int)));
-    QObject::connect(&futureWatcher, SIGNAL(progressValueChanged(int)), &dialog, SLOT(setValue(int)));
+        // Create a QFutureWatcher and connect signals and slots.
+        QFutureWatcher<void> futureWatcher;
+        QObject::connect(&futureWatcher, SIGNAL(finished()), &dialog, SLOT(reset()));
+        QObject::connect(&dialog, SIGNAL(canceled()), &futureWatcher, SLOT(cancel()));
+        QObject::connect(&futureWatcher, SIGNAL(progressRangeChanged(int,int)), &dialog, SLOT(setRange(int,int)));
+        QObject::connect(&futureWatcher, SIGNAL(progressValueChanged(int)), &dialog, SLOT(setValue(int)));
 
-    // Start the computation.
-    futureWatcher.setFuture(QtConcurrent::run(read, fileName, converter, node, shell));
+        // Start the computation.
+        futureWatcher.setFuture(QtConcurrent::run(read, fileName, converter, node, shell));
 
-    // Display the dialog and start the event loop.
-    dialog.exec();
+        // Display the dialog and start the event loop.
+        dialog.exec();
 
-    futureWatcher.waitForFinished();
+        futureWatcher.waitForFinished();
 
-    // Query the future to check if was canceled.
-    qDebug() << "Canceled?" << futureWatcher.future().isCanceled();
+        // Query the future to check if was canceled.
+        qDebug() << "Canceled?" << futureWatcher.future().isCanceled();
+    }
+    else
+    {
+        QMessageBox::information(this, tr("Info"), "The document has not been loaded.");
+    }
 
     //--------------------------------------------------------------------------------------<
-//    if(fileName != NULL)
-//    {
-//        //initilalize the counter line for progress bar
-//        int CountNumberLine = 0;
+    //    if(fileName != NULL)
+    //    {
+    //        //initilalize the counter line for progress bar
+    //        int CountNumberLine = 0;
 
-//        //update progress bar
-//        //ui->progressBar->setMaximum(countLineNumber(fileName));
-//        //update text bar input file
-//        ui->lineEdit->setText(fileName);
+    //        //update progress bar
+    //        //ui->progressBar->setMaximum(countLineNumber(fileName));
+    //        //update text bar input file
+    //        ui->lineEdit->setText(fileName);
 
-//        //read file
-//        QFile inputFile(fileName);
+    //        //read file
+    //        QFile inputFile(fileName);
 
-//        //update group information file
-//        double sizeFile = inputFile.size()/(1048576);
-//        ui->label->setText("Dimension: " + QString().setNum(sizeFile,'d',2) + " Mb");
+    //        //update group information file
+    //        double sizeFile = inputFile.size()/(1048576);
+    //        ui->label->setText("Dimension: " + QString().setNum(sizeFile,'d',2) + " Mb");
 
-//        //open file
-//        if (inputFile.open(QIODevice::ReadOnly | QIODevice::Text))
-//        {
-//            QTextStream in(&inputFile);
-//            while (!in.atEnd())
-//            {
-//                QString line = in.readLine();
-//                CountNumberLine += 1;
+    //        //open file
+    //        if (inputFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    //        {
+    //            QTextStream in(&inputFile);
+    //            while (!in.atEnd())
+    //            {
+    //                QString line = in.readLine();
+    //                CountNumberLine += 1;
 
-//                //store data
-                //converter->setInputLine(futureWatcher.result(), test, testshell);
+    //                //store data
+    //converter->setInputLine(futureWatcher.result(), test, testshell);
 
-//                //refresh progress bar
-//                ui->progressBar->setValue(CountNumberLine);
-//            }
-//        }
+    //                //refresh progress bar
+    //                ui->progressBar->setValue(CountNumberLine);
+    //            }
+    //        }
 
-        //close file
-        //inputFile.close();
+    //close file
+    //inputFile.close();
 
-        //active conversion
-        ui->Convert->setEnabled(true);
-        ui->lineEdit_2->setText(fileName);
+    //active conversion
+    ui->Convert->setEnabled(true);
+    ui->lineEdit_2->setText(fileName);
     //}
     //int *numnode = new int(test->getNodeStructure().size());
     //ui->Nodeinfo->setText("Total number node: " + QString(*numnode) );
