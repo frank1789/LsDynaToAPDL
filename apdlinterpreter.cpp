f4bca6c1758a8972e6901cf985504069b7b485e1
Francesco <francesco.argentieri89@gmail.com>
2017-09-10 10:06:19 +0200

    minor correction

--- apdlinterpreter.cpp
+++ apdlinterpreter.cpp
@@ -1,97 +1,13 @@
 #include "apdlinterpreter.h"
 #include "lsdynainterpreter.h"
-/*
-APDLsintax::ApdlInterpreter::~ApdlInterpreter()
-{
-    qDebug() << "Pure virtual destructor is called for ApdlInterpreter()";
-}
-
-APDLsintax::Node::Node(QVector<NodeProperty> pVectorNode)
-{
-    qDebug()<<"Call constructor Node()";
-    _NodeToWrite = pVectorNode;
-    _NodeStringOutput.clear();
-
-    //make header Apdl File command
-    _header = "FINISH\n";
-    _header += "/CLEAR,START,NEW\n\n";
-    _header += "/PREP7\n\n";
-}
-
-void APDLsintax::Node::setWriter()
-{
-    for(int it = 0; it < _NodeToWrite.size(); it++)
-    {
-        //node string N, command to define node ',' is field separetor
-        _NodeStringOutput = "N,";
-
-        //set number of node
-        _NodeStringOutput += _NodeToWrite[it].id + ',';
-
-        //set coordinate x
-        _NodeStringOutput += _NodeToWrite[it].cordinatex + ',';
-
-        //set coordinate y
-        _NodeStringOutput += _NodeToWrite[it].cordinatey + ',';
-
-        //set coordinate z
-        _NodeStringOutput += _NodeToWrite[it].cordinatez + '\n';
-        qDebug()<<_NodeStringOutput;
-    }
-    qDebug()<<"End make node command APDL!";
-}
-
-APDLsintax::Node::~Node()
-{
-    qDebug()<<"Called ~Node()";
-
-}
-
-APDLsintax::ElementShell::ElementShell()
-{
-    qDebug() << "Call constructor ElementShell()";
-    a = 0;
-}
-
-void APDLsintax::ElementShell::setWriter()
-{
-    qDebug() << "success!";
-
-    sect,1229203,shell,,
-    secdata,3.734283,1,0.0,3
-    secoffset,MID
-    seccontrol,,,, , , ,
-
-    TYPE, 1
-    MAT, 1
-    REAL,
-    ESYS, 0 SECNUM, 1229203
-    E,1189369,1188878,1188879,1189370
 
 
-    "SECT," + id + ",SHELL,,";
-    "SECTDATA, " + thickness + ",1,0.0,3";
-    "SECOFFEST,MID"
-    "SECCONTROL,,,, , , ,"
-    "TYPE,1
-    "MAT,1
-    "REAL,"
-    "ESYS, 0 SECNUM," + id
-    "E,"+ node1+ "," + node2+ ","+ node3+","+node4
-
-}
-
-APDLsintax::ElementShell::~ElementShell()
-{
-    qDebug() <<"Called ~ElementShell()";
-}
-*/
 void APDLsintax::Writer(QString pfilename, QVector<NodeProperty> pVectorNode, QVector<ShellProperty> pVectorElementShell)
 {
-    qDebug()<<"Called Writer()";
+    qDebug()<<"Called Writer() function";
 
     QFile file( pfilename );
-    if ( file.open(QIODevice::WriteOnly) )
+    if ( file.open(QIODevice::ReadWrite) )
     {
         QTextStream stream( &file );
 
@@ -138,6 +54,11 @@ void APDLsintax::Writer(QString pfilename, QVector<NodeProperty> pVectorNode, QV
         //write element
         for(int it = 1; it < pVectorElementShell.size() ; ++it)
         {
+            qDebug()<< "Element Id:" << pVectorElementShell.at(it).id_element <<", E," << pVectorElementShell.at(it).node1 << ","
+                    << pVectorElementShell.at(it).node2 << "," << pVectorElementShell.at(it).node3 << ","
+                    << pVectorElementShell.at(it).node4 << "thickness element:" << pVectorElementShell.at(it).thickness;
+
+            //print on file shell element vector
             stream << "SECT," + pVectorElementShell[it].id_element + ",SHELL,," + "\n";
             stream << "SECTDATA, " + pVectorElementShell[it].thickness + ",1,0.0,3"+ "\n";
             stream << "SECOFFEST,MID"  << endl;
@@ -146,8 +67,10 @@ void APDLsintax::Writer(QString pfilename, QVector<NodeProperty> pVectorNode, QV
             stream << "MAT,1"  << endl;
             stream << "REAL,"  << endl;
             stream << "ESYS, 0 SECNUM," + pVectorElementShell[it].id_element + "\n\n";
-            stream << "E,"+ pVectorElementShell[it].node1 + "," + pVectorElementShell[it].node2 + "," + pVectorElementShell[it].node3 + "," + pVectorElementShell[it].node4 + "\n\n\n";
+            stream << "E,"+ pVectorElementShell[it].node1 + "," + pVectorElementShell[it].node2 + "," +
+                      pVectorElementShell[it].node3 + "," + pVectorElementShell[it].node4 + "\n\n\n";
         }
+
         qDebug()<<"End make element shell command APDL!";
     }
 
