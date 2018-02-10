7d0e9b2d6a13492db35b300a0a8a64e4d8803d88
Francesco <francesco.argentieri89@gmail.com>
2018-02-10 12:16:49 +0100

    add documentation

--- finitelement.cpp
+++ finitelement.cpp
@@ -2,6 +2,9 @@
 #include "QDebug"
 
 template<>
+/**
+ * @brief The propnode<int, double> struct specializzation @see propnode
+ */
 struct propnode<int, double>
 {
   int idnode;
@@ -11,13 +14,28 @@ struct propnode<int, double>
 };
 
 template<>
+/**
+ * @brief The propelem<int, int, double, 4> struct specializzation
+ * element 4-node shell @see propelem
+ *
+ * @details This implementation uses 4-node shell elements with thickness, is used
+ * to represent the SHELL181 element present in Ansys.
+ */
 struct propelem<int, int, double, 4>
 {
-  int idelem;          /**< */
-  int nodeelem[4];    /**< */
-  double  elemthick;   /**< */
+  int idelem;
+  int nodeelem[4];
+  double elemthick;
 };
 
-
+/**
+ * @brief FiniteElement::~FiniteElement default distructor abstract class.
+ *
+ */
 FiniteElement::~FiniteElement() { qDebug() << "call ~FiniteElement()";}
+
+/**
+ * @brief Element::~Element default distructor class.
+ *
+ */
 Element::~Element() {qDebug() << "call ~Element()";}
--- finitelement.h
+++ finitelement.h
@@ -3,25 +3,62 @@
 #include <QString>
 
 template<typename N, typename P>
+/**
+ * @brief The propnode struct contains the information of a single node.
+ *
+ * @details The information is divided into: the ID of the node that
+ * uniquely identifies it, the x coordinate, the y coordinate, the z coordinate.
+ * We use a template definition to be able to create multiple definitions
+ * depending on the nodes used.
+ * It is suggested to use integer data types to define node IDs, while
+ * for float data type  coordinates.
+ *
+ * @param N type for node IDs
+ * @param P type for spatial coordinate
+ */
 struct propnode
 {
-  N idnode; /**< */
-  P coordinate_x; /**< */
-  P coordinate_y; /**< */
-  P coordinate_z; /**< */
+  N idnode; /**< uniquely identifies node IDs */
+  P coordinate_x; /**< x coordinate in the space */
+  P coordinate_y; /**< y coordinate in the space */
+  P coordinate_z; /**< z coordinate in the space */
 };
 
 template<typename N, typename EN, typename T, int size>
+/**
+ * @brief The propelem struct defines the general appearance of the parameters
+ * of an element.
+ *
+ * @details The content of the structure represents the element defined by a
+ * unique ID, by the number of nodes that constitute it. The thickness used for
+ * shell-like elements.
+ * A template formulation is used to make most of the definitions available to
+ * the structure.
+ * It is suggested to use integer data types:
+ * - element IDs;
+ * - nodes that constitute it as identified by the ID;
+ * - number of nodes of the element;\n
+ * Float Data type:
+ * - thickness.
+ *
+ * @param N type for elments IDs
+ * @param EN type for node IDs
+ * @param T type for element's thickness
+ * @param size sets the number of nodes of the element
+ */
 struct propelem
 {
-  N idelem;           /**< */
-  EN nodeelem[size];  /**< */
-  T  elemthick;       /**< */
+  N idelem;           /**< uniquely identifies element IDs */
+  EN nodeelem[size];  /**< ID of the node that constitutes the element */
+  T  elemthick;       /**< thickness of the element */
 };
 
 
 /**
- * @brief The FiniteElement class
+ * @brief The FiniteElement class to define the type of elements and their structure
+ *
+ * @details provides virtual methods to be defined in the derived classes to
+ * better interpret the type of existing finite elements.
  */
 class FiniteElement
 {
@@ -31,19 +68,19 @@ public:
   virtual ~FiniteElement() = 0;
 };
 
-
+/**
+ * @brief The Element class defines the information common to the type of element
+ *
+ */
 class Element : public FiniteElement
 {
 public:
   ~Element() = 0;
   void setMaterial();
 protected:
-  double _moduleYoung;  /**< */
-  double _poissonCoeff; /**< */
+  double _moduleYoung;  /**< value of the elastic modulus of the material */
+  double _poissonCoeff; /**< value of the poisson coefficient */
   int _numofnode; /**< */
 };
-
-
-
 
 #endif // FINITELEMENT_H
