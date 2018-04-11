1a1b3babc056d959557824808a28bba58df93f7b
Francesco <francesco.argentieri89@gmail.com>
2018-04-11 16:46:32 +0200

    manager file separated

---
+++ managefile.cpp
@@ -0,0 +1,6 @@
+#include "managefile.h"
+
+ManageFile::ManageFile()
+{
+
+}
---
+++ managefile.h
@@ -0,0 +1,12 @@
+#ifndef MANAGEFILE_H
+#define MANAGEFILE_H
+
+#include <QObject>
+
+class ManageFile
+{
+public:
+    ManageFile();
+};
+
+#endif // MANAGEFILE_H
