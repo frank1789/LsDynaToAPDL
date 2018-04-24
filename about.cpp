3a8328352a129623afecdd7b5f5925c395803ccb
Francesco <francesco.argentieri89@gmail.com>
2018-04-24 10:58:11 +0200

    :bug: version are void
    search new trick to retrive git tag

--- about.cpp
+++ about.cpp
@@ -1,47 +1,62 @@
 #include "about.h"
 #include "ui_about.h"
 
+#define VERSION 1
+#define BUILD 12523
+
 
 About::About(QWidget *parent) :
-  QDialog(parent),
-  ui(new Ui::About)
+    QDialog(parent),
+    ui(new Ui::About)
 {
-  //define window property
-  ui->setupUi(this);
-  this->setWindowTitle("");
-  //define plain text object and content
-  ui->plainTextEdit->setReadOnly(true);
-  ui->plainTextEdit->appendPlainText(read(":/Resources/Resources/License/License.txt"));
-  //set image property
-  QPixmap imageObject(":/Resources/Resources/Icon/generic.png");
-  imageObject.scaled(120,120);
-  //define visualization
-  ui->logo->setPixmap(imageObject);
-  // add author
-  ui->author->setText(QString("LsDynaToAPDL\nFrancesco Argentieri"));
-  //add information builder
-  ui->version->setText(QString("Version %1").arg(VERSION));
-  ui->build->setText(QString("Build %1").arg(BUILD));
-  qDebug() << VERSION;
-  qDebug() << BUILD;
+    //define window property
+    ui->setupUi(this);
+    this->setWindowTitle("");
+    //define plain text object and content
+    ui->plainTextEdit->setReadOnly(true);
+    ui->plainTextEdit->appendPlainText(read(":/Resources/Resources/License/License.txt"));
+    //set image property
+    QPixmap imageObject(":/Resources/Resources/Icon/generic.png");
+    imageObject.scaled(120,120);
+    //define visualization
+    ui->logo->setPixmap(imageObject);
+    // add author
+    ui->author->setText(QString("LsDynaToAPDL\nFrancesco Argentieri"));
+    //add information builder
+    ui->version->setText(QString("Version %1").arg(VERSION));
+    ui->build->setText(QString("Build %1").arg(BUILD));
+    qDebug() << VERSION;
+    qDebug() << BUILD;
 }
 
 About::~About()
 {
-  delete ui;
+    delete ui;
+}
+
+void About::closeEvent(QCloseEvent *event)
+{
+    if(event)
+    {
+        emit dialogClosed();
+        event->accept();
+        close();
+    }
+    else
+        event->ignore();
 }
 
 QString read(QString pfileName)
 {
-  QFile fileInput(pfileName);
-  if(!fileInput.open(QFile::ReadOnly | QFile::Text))
+    QFile fileInput(pfileName);
+    if(!fileInput.open(QFile::ReadOnly | QFile::Text))
     {
-      qDebug() << "failed to open file";
-      return NULL;
+        qDebug() << "failed to open file";
+        return NULL;
     }
-  QTextStream text(&fileInput);
-  QString all = text.readAll();
-  qDebug() << all;
-  fileInput.close();
-  return all;
+    QTextStream text(&fileInput);
+    QString all = text.readAll();
+    qDebug() << all;
+    fileInput.close();
+    return all;
 }
