e5a60c0bd50d7821868b7054948fc607b8d79ef1
Francesco <francesco.argentieri89@gmail.com>
2020-11-16 14:57:48 +0100

    Add test for filemanager class
    Added test for all methods

--- test/test_managerfile.cc
+++ test/test_managerfile.cc
@@ -1,25 +1,21 @@
 #include "filemanager.h"
 #include "gtest/gtest.h"
 
-
-#include <QString>
-
 TEST(Creation, ManageFile) {
   auto manager = ManageFile();
   EXPECT_NO_THROW(ManageFile());
-  
 }
 
 TEST(ManageFile, Setup) {
   auto manager = ManageFile();
-  manager.processedFilename("testnofile.k");
-  EXPECT_EQ(manager.getFilename(), QString("testnofile.k"));
+  manager.processedFilename("path/to/folder/testnofile.k");
+  EXPECT_EQ(manager.getFilename(), "testnofile.k");
 }
 
-TEST(ManageFile, UpdateName){
+TEST(ManageFile, UpdateName) {
   auto manager = ManageFile();
-  manager.processedFilename("EmptyFile.k");
+  manager.processedFilename("path/to/folder/EmptyFile.k");
   EXPECT_EQ(manager.getFilename(), QString("EmptyFile.k"));
-  EXPECT_EQ(manager.getOutputfile(), QString("ApdlConverted.txt"));
-  EXPECT_EQ(manager.getOutputfile(), QString("EmptyFileApdlConverted.txt"));
+  EXPECT_EQ(manager.getOutputfile(),
+            QString("path/to/folder/EmptyFile_converted.txt"));
 }
