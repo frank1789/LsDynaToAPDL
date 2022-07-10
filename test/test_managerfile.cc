2e53c047476f0ef0f5de9cc5410473545eed192a
Francesco <francesco.argentieri89@gmail.com>
2022-07-10 18:31:52 +0200

    Add tests

--- test/test_managerfile.cc
+++ test/test_managerfile.cc
@@ -3,16 +3,23 @@
 
 static constexpr char kExampleFile[] = "../bin/example.k";
 
-TEST(Creation, FileManager) { EXPECT_NO_THROW(FileManager()); }
+TEST(Creation, FileManager) {
+  EXPECT_NO_THROW(FileManager());
+}
 
 auto s_manager = FileManager();
 
 TEST(FileManager, ExistFile) {
+  EXPECT_FALSE(FileManager::isValidFile("../bin/example2.k"));
   EXPECT_TRUE(FileManager::isValidFile(kExampleFile));
 }
 
 TEST(FileManager, setNewFilename) {
   s_manager.setFilename(kExampleFile);
+  EXPECT_EQ(s_manager.getFilename(), QStringLiteral("example.k"));
+  s_manager.setFilename(std::string(kExampleFile));
+  EXPECT_EQ(s_manager.getFilename(), QStringLiteral("example.k"));
+  s_manager.setFilename(QString(kExampleFile));
   EXPECT_EQ(s_manager.getFilename(), QStringLiteral("example.k"));
   // EXPECT_EQ(s_manager.getCompleteFilename(), QString(kExampleFile));
 }
