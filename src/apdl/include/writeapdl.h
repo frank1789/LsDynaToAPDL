ead3b34ef648d0dda603e66ec691d822fbb04cd8
Francesco <francesco.argentieri89@gmail.com>
2022-08-08 11:37:11 +0200

    Add thread writer

--- src/apdl/include/writeapdl.h
+++ src/apdl/include/writeapdl.h
@@ -1,29 +1,59 @@
+/**
+ * @file writeapdl.h
+ * @author Francesco Argentieri (francesco.argentieri89@gmail.com)
+ * @brief Writer is a class that writes information within APDL syntax.
+ * @version 0.1
+ * @date 2022-08-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #ifndef WRITE_APDL_H
 #define WRITE_APDL_H
 
-// #include <QString>
-
-// #include "node.h"
-// #include "shell.h"
-
-// void writeAPDL(const QString &FileName, Node *node, Shell *shell);
-
 #include <QFile>
 #include <QScopedPointer>
 #include <QString>
+#include <QThread>
 
 namespace apdl {
 
-class Writer {
+class Writer : public QThread {
+  Q_OBJECT
  public:
-  explicit Writer(const QString& filename);
-  ~Writer();
+  /**
+   * @brief Construct a new Writer object
+   *
+   * @param parent QObject parent pointer
+   */
+  explicit Writer(QObject *parent = nullptr);
 
+  /**
+   * @brief Construct a new Writer object
+   *
+   * @param filename QString holds output filename
+   * @param parent QObject parent pointer
+   */
+  explicit Writer(const QString &filename, QObject *parent = nullptr);
+
+  /**
+   * @brief Elaborate file.
+   *
+   */
+  void run() override;
+
+  /**
+   * @brief Set the Filename output.
+   *
+   * @param filename QString hold the entire path to output file.
+   */
+  void setFilename(const QString &filename);
+
+ private:
   void writeNode();
   void writeElement();
 
- private:
   QString filename_;
   QScopedPointer<QFile> ptr_file_{nullptr};
 };
--- src/apdl/src/writeapdl.cc
+++ src/apdl/src/writeapdl.cc
@@ -1,22 +1,60 @@
+/**
+ * @file writeapdl.cc
+ * @author Francesco Argentieri (francesco.argentieri89@gmail.com)
+ * @brief Writer is a class that writes information within APDL syntax.
+ * @version 0.1
+ * @date 2022-08-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include "writeapdl.h"
 
 #include <QDebug>
 #include <QFileDevice>
 #include <QTextStream>
 
+#include "logger_tools.h"
+
 namespace apdl {
 
-Writer::Writer(const QString& filename) : filename_(filename) { ptr_file_.reset(new QFile(filename_)); }
+Writer::Writer(QObject *parent) : QThread(parent) { wait(); }
 
-Writer::~Writer() { ptr_file_->close(); }
+Writer::Writer(const QString &filename, QObject *parent) : QThread(parent), filename_(filename) {
+  wait();
+  ptr_file_.reset(new QFile(filename_));
+}
+
+void Writer::setFilename(const QString &filename) {
+  if (!filename.isEmpty()) {
+    filename_ = filename;
+  }
+}
+
+void Writer::run() {
+  if (!filename_.isEmpty()) {
+    if (ptr_file_ == nullptr) {
+      ptr_file_.reset(new QFile(filename_));
+      if (ptr_file_->open(QIODevice::WriteOnly | QIODevice::Text)) {
+        /* Do something */
+      }
+
+      ptr_file_->close();
+    }
+  } else {
+    qWarning().noquote() << INFOFILE << "invalid or empty filename, please use setFilename(const QString &filename)";
+    quit();
+  }
+}
 
 void Writer::writeNode() {
   if (ptr_file_->open(QIODevice::WriteOnly | QIODevice::Append)) {
     QTextStream stream(ptr_file_.get());
     stream << "/PREP7\n\n";
-    // wtite node section
+    // write node section
     stream << "! >>>>> node definition <<<<<\n\n";
-    //       // node format N, NODE, X, Y, Z, THXY, THYZ, THZX
+    // node format N, NODE, X, Y, Z, THXY, THYZ, THZX
     stream << "\n\n! >>>>> end node definition <<<<<\n\n";
   }
 
