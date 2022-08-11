0d5e876ed58fbd82772545186b6654743067b88e
Francesco <francesco.argentieri89@gmail.com>
2022-08-11 17:12:36 +0200

    Fix parser threads co-operations

--- src/core/parser.cc
+++ src/core/parser.cc
@@ -12,41 +12,143 @@
 #include "parser.h"
 
 #include <QDebug>
+#include <QGridLayout>
+#include <QLabel>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QTime>
+#include <QTimer>
 
 #include "logger_tools.h"
-#include "writeapdl.h"
 
 namespace core {
 
-Parser::Parser(QObject* parent) :
-    QObject(parent), filemanager_(new FileManager), converter_(new ConverterDialog), writer_(new apdl::WriterDialog) {
-  QObject::connect(converter_.get(), &ConverterDialog::closed, this, [=]() {
-    qDebug().noquote() << INFOFILE << "check for the next file";
-    emit finished();
-  });
+constexpr uint8_t kTimeInterval{10};  // [ms]
+static const auto kTitleIncipit = QStringLiteral("Processing file: ");
 
-  //  QObject::connect(converter_.get(), &ConverterDialog::closed, this,
-  //  [=](){});
-
-  QObject::connect(this, &Parser::finished, this, &Parser::writeToFile);
+template <typename T>
+inline void safe_delete(T *&pointer) {
+  if (pointer != nullptr) {
+    delete pointer;
+    pointer = nullptr;
+  }
 }
 
-void Parser::elaborateFilename(const QString& filename) {
+Parser::Parser(QWidget *parent) :
+    QDialog(parent),
+    filemanager_(new FileManager),
+    converter_(new syntax::lsdyna::ConverterSyntax),
+    writer_(new apdl::Writer),
+    timer_(new QTimer),
+    elapsed_time_(new QTime(0, 0, 0, 0)) {
+  setWindowFlag(Qt::WindowStaysOnTopHint);
+  this->setWindowTitle(kTitleIncipit);
+  this->setupLayout();
+
+  QObject::connect(cancel_btn_, &QPushButton::clicked, this, [this]() {
+    qWarning().noquote() << INFOFILE << "cancel requested";
+    //    worker_->stopOperations();
+    timer_->stop();
+    close();
+    *elapsed_time_ = QTime(0, 0, 0, 0);
+    // emit closed();
+  });
+
+  timer_->setInterval(kTimeInterval);
+
+  QObject::connect(converter_.get(), &syntax::lsdyna::ConverterSyntax::finished, this, &Parser::writeToFile);
+  QObject::connect(writer_.get(), &apdl::Writer::finished, this, [this]() {
+    qDebug() << INFOFILE << "finished";
+    close();
+  });
+}
+
+Parser::~Parser() {
+  safe_delete(grid_layot_);
+  safe_delete(in_file_label_);
+  safe_delete(in_filename_label_);
+  safe_delete(out_file_label_);
+  safe_delete(out_filename_label_);
+  safe_delete(elapsedtime_label_);
+  safe_delete(time_label_);
+  safe_delete(label_);
+  safe_delete(pbar_);
+  safe_delete(cancel_btn_);
+  safe_delete(elapsed_time_);
+  safe_delete(timer_);
+}
+
+void Parser::showEvent(QShowEvent *event) {
+  qDebug().noquote() << INFOFILE << "ui has shown then execute thread";
+  timer_->start();
+  converter_->start();
+  Q_UNUSED(event)
+}
+
+void Parser::closeEvent(QCloseEvent *event) {
+  qDebug().noquote() << INFOFILE << "ui has closed then wait thread";
+  timer_->stop();
+  Q_UNUSED(event)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Layout
+///////////////////////////////////////////////////////////////////////////////
+
+void Parser::setupLayout() {
+  grid_layot_ = new QGridLayout(this);
+
+  in_file_label_ = new QLabel(QStringLiteral("input file: "));
+  out_file_label_ = new QLabel(QStringLiteral("output file: "));
+  elapsedtime_label_ = new QLabel(QStringLiteral("Elapsed time: "));
+  time_label_ = new QLabel(elapsed_time_->toString("hh:mm:ss.zzz"));
+  in_filename_label_ = new QLabel("");
+  out_filename_label_ = new QLabel("");
+
+  // setup progress-bar
+  pbar_ = new QProgressBar;
+  pbar_->setMinimum(0);
+  pbar_->setMaximum(0);
+
+  // setup button
+  cancel_btn_ = new QPushButton;
+  cancel_btn_->setText(QStringLiteral("cancel"));
+
+  // setup grid
+  grid_layot_->addWidget(in_file_label_, 0, 0, 1, 1);
+  grid_layot_->addWidget(in_filename_label_, 0, 1, 1, 3);
+  grid_layot_->addWidget(out_file_label_, 1, 0, 1, 1);
+  grid_layot_->addWidget(out_filename_label_, 1, 1, 1, 3);
+  grid_layot_->addWidget(elapsedtime_label_, 2, 0, 1, 1);
+  grid_layot_->addWidget(time_label_, 2, 1, 1, 3);
+  grid_layot_->addWidget(pbar_, 3, 0, 1, 5);
+  grid_layot_->addWidget(cancel_btn_, 4, 4, 1, 1);
+
+  QObject::connect(timer_, &QTimer::timeout, this, [=]() {
+    *elapsed_time_ = elapsed_time_->addMSecs(kTimeInterval);
+    time_label_->setText(elapsed_time_->toString("hh:mm:ss.zzz"));
+  });
+}
+
+void Parser::elaborateFilename(const QString &filename) {
   if (filename.isEmpty()) {
     qWarning().noquote() << INFOFILE << "[WARNING] invalid file:" << filename;
     return;
   }
 
   filemanager_->setFilename(filename);
-  converter_->setInputFile(filename);
-  converter_->exec();
-  emit finished();
+  this->setWindowTitle(kTitleIncipit + filemanager_->getFilename());
+  in_filename_label_->setText(filemanager_->getCompleteFilename());
+  out_filename_label_->setText(filemanager_->getOutputFile());
+
+  converter_->setInputFile(filemanager_->getCompleteFilename());
+  writer_->setFilename(filemanager_->getOutputFile());
+  this->exec();
 }
 
 void Parser::writeToFile() {
-  qDebug().noquote() << INFOFILE << "ready to write on file:" << filemanager_->getOutputfile();
-  writer_->setOutputFilename(filemanager_->getOutputfile());
-  writer_->exec();
+  qDebug().noquote() << INFOFILE << "ready to write on file:" << filemanager_->getOutputFile();
+  writer_->start();
 }
 
 }  // namespace core
--- src/core/parser.h
+++ src/core/parser.h
@@ -1,8 +1,8 @@
 /**
  * @file parser.h
  * @author Francesco Argentieri (francesco.argentieri89@gmail.com)
- * @brief The parser class holds the components and permit the transfomation for
- * both syntax.
+ * @brief The parser class holds the components and permit the transformation
+ * for both syntax.
  * @version 0.1
  * @date 2022-08-01
  *
@@ -13,34 +13,59 @@
 #ifndef LSDYNA_TO_APDL_PARSER_H
 #define LSDYNA_TO_APDL_PARSER_H
 
-#include <QObject>
+#include <QDialog>
 #include <QScopedPointer>
 
-#include "converterdialog.h"
+#include "converter.h"
 #include "filemanager.h"
-#include "writerdialog.h"
+#include "writeapdl.h"
+
+QT_BEGIN_NAMESPACE
+class QGridLayout;
+class QProgressBar;
+class QLabel;
+class QPushButton;
+class QTime;
+class QTimer;
+QT_END_NAMESPACE
 
 namespace core {
 
-class Parser : public QObject {
+class Parser : public QDialog {
   Q_OBJECT
 
  public:
-  explicit Parser(QObject* paren = nullptr);
+  explicit Parser(QWidget *parent = nullptr);
+  ~Parser() override;
 
  public slots:
-  void elaborateFilename(const QString& filename);
+  void elaborateFilename(const QString &filename);
   void writeToFile();
 
  signals:
-  void elaborationFinished(bool);
-  void elaborationCanceled(bool);
-  void finished();
+
+ protected:
+  void showEvent(QShowEvent *event) override;
+  void closeEvent(QCloseEvent *event) override;
 
  private:
   QScopedPointer<FileManager> filemanager_{nullptr};
-  QScopedPointer<ConverterDialog> converter_{nullptr};
-  QScopedPointer<apdl::WriterDialog> writer_{nullptr};
+  QScopedPointer<syntax::lsdyna::ConverterSyntax> converter_{nullptr};
+  QScopedPointer<apdl::Writer> writer_{nullptr};
+
+  void setupLayout();
+  QGridLayout *grid_layot_{nullptr};
+  QLabel *in_file_label_{nullptr};
+  QLabel *out_file_label_{nullptr};
+  QLabel *in_filename_label_{nullptr};
+  QLabel *out_filename_label_{nullptr};
+  QLabel *elapsedtime_label_{nullptr};
+  QLabel *time_label_{nullptr};
+  QLabel *label_{nullptr};
+  QProgressBar *pbar_{nullptr};
+  QPushButton *cancel_btn_{nullptr};
+  QTimer *timer_{nullptr};
+  QTime *elapsed_time_{nullptr};
 };
 
 }  // namespace core
