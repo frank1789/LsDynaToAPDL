bd18c862b2aad517ec3242917d4f60797f0d1fd5
Francesco <francesco.argentieri89@gmail.com>
2022-10-03 17:02:01 +0200

    Fix style and minor problems

--- test/test_logger.cc
+++ test/test_logger.cc
@@ -8,7 +8,5 @@ std::string dummyfunc() { return INFOFILE.toStdString(); };
 TEST(Logger, Logger) {
   auto message = dummyfunc();
   EXPECT_EQ(message,
-            "std::string dummyfunc() "
-            "/Users/francesco/Documents/Project/LsDynaToAPDL/test/"
-            "test_logger.cc:6] ");
+            "std::string dummyfunc() /Users/francesco/Documents/Project/LsDynaToAPDL/test/test_logger.cc:6]");
 }
